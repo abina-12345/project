e973127c9f5b135bb1c6c7b8c783fe5948faae98
abinash <dj.avinash55@gmail.com>
2022-04-17 12:12:31 +0530

    new test

--- src/test/java/test/Test1.java
+++ src/test/java/test/Test1.java
@@ -7,9 +7,9 @@ public class Test1 {
 		int a=10;
 		int b=20;
 		
-		
-		System.out.println(a+b);
-		System.out.println(a-b);
+		//Multiply and Division
+		System.out.println(a*b);
+		System.out.println(a/b);
 
 	}
 
