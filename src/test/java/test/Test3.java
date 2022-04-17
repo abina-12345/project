0738d5df669ffb27989f14bfbc1bc83b0a97b634
abinash <dj.avinash55@gmail.com>
2022-04-17 12:30:46 +0530

    3rdTEST

---
+++ src/test/java/test/Test3.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		//Multiply and Division
+		System.out.println(a*b);
+		System.out.println(a/b);
+
+	}
+
+}
